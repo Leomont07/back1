7aa234b5ba483c36a3d91914704df5d4a9cd9077
Leomont07 <monteroleon52@gmail.com>
2025-04-02 21:49:05 -0600

    verificacion

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -136,7 +136,7 @@ router.post('/verify-mfa', async (req, res) => {
             return res.status(400).json({ message: 'Invalid OTP' });
         }
 
-        const token = jwt.sign({ id: userId, email: userData.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: userId, email: userData.email, grado: userData.grado, grupo: userData.grupo }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         console.log('JWT generado');
 
@@ -145,6 +145,7 @@ router.post('/verify-mfa', async (req, res) => {
         res.status(500).json({ message: 'Server error', error: error.message });
     }
 });
+
 
 // GET /logs
 router.get('/logs', async (req, res) => {
