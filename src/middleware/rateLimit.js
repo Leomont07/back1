01d3be8212c71bd3462b0cf25a29154301946475
Leomont07 <monteroleon52@gmail.com>
2025-04-02 23:45:50 -0600

    limit

--- src/middleware/rateLimit.js
+++ src/middleware/rateLimit.js
@@ -6,7 +6,7 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again after 10 minutes',
   keyGenerator: (req) => req.ip,
   handler: (req, res, next) => {
-    const server2Url = 'https://back2-jlgc.onrender.com';
+    const server2Url = 'https://back2-7697.onrender.com/';
     const redirectUrl = `${server2Url}${req.originalUrl}`;
     res.redirect(307, redirectUrl);
   },
